6f09c2b4bcbe97d32b153d814565d5b4446a8722
GS_song <105411445+SongGwanSeok@users.noreply.github.com>
2022-09-27 13:51:11 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 68 ms, Memory: 2556 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.c
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.c
@@ -1,49 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct {
-	int x;
-	int y;
-}coord;
-
-int compare(const void* first, const void* second)
+#include<stdio.h>
+#include<stdlib.h>
+struct point {
+    int x ;
+    int y ;
+};
+int compare(const void *a, const void *b)    // 오름차순 비교 함수 구현
 {
-	coord* a = (coord*)first;
-	coord* b = (coord*)second;
+    struct point num1 = *(struct point *)a;    // void 포인터를 int 포인터로 변환한 뒤 역참조하여 값을 가져옴
+    struct point num2 = *(struct point *)b;    // void 포인터를 int 포인터로 변환한 뒤 역참조하여 값을 가져옴
 
-	if (a->y < b->y)
-		return -1;
-	else if (a->y > b->y)
-		return 1;
-	else
-	{
-		if (a->x < b->x)
-			return -1;
-		else
-			return 1;
-	}
-	return 0;
+    if (num1.y < num2.y)    // a가 b보다 작을 때는
+        return -1;   // -1 반환
+    else if(num1.y > num2.y)
+        return 1;
+    else{
+        if(num1.x<num2.x)
+            return -1;
+        else
+            return 1;
+    }
+
+    return 0;    // a와 b가 같을 때는 0 반환
 }
 
-int main()
-{
-	int i, n;
-	coord* list;
-
-	scanf("%d", &n);
-	list = (coord*)malloc(n * sizeof(coord));
-
-	for (i = 0; i < n; i++)
-	{
-		scanf(" %d %d", &list[i].x, &list[i].y);
-	}
-
-	qsort(list, n, sizeof(list[0]), compare);
+int main(){
+    struct point arr[100001] ;
+    struct point temp;
+    int temp2;
 
 
-	for (i = 0; i < n; i++)
-	{
-		printf("%d %d\n", list[i].x, list[i].y);
-	}
-	return 0;
+    int N;
+    scanf("%d", &N);
+    for(int i =0;i<N;i++){
+        scanf("%d %d", &arr[i].x, &arr[i].y);
+    }
+    qsort(arr, N, sizeof(arr[0]), compare);
+
+
+    for(int i=0;i<N;i++){
+        printf("%d %d\n", arr[i].x, arr[i].y );
+    }
+
+    return 0;
 }
