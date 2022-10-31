446e0247a63099d6a720a127d5de0fe4c3ccc02a
GS_song <105411445+SongGwanSeok@users.noreply.github.com>
2022-10-31 09:01:16 +0900

    [Bronze IV] Title: Hello Judge, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main(){
+
+    int N=0;
+    scanf("%d", &N);
+
+    for(int i=1;i<=N;i++){
+        printf("Hello World, Judge %d!\n", i);
+    }
+
+    return 0;
+}
